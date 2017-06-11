0c087a36800c5e711ba1e5522984c68e4d8f1bfc
chenzhu <c54948054@126.com>
2017-06-12 00:21:13 +0800

    fill field type

--- models/model.go
+++ models/model.go
@@ -1,32 +1,58 @@
 package models
 
-type FieldTyp string
+type FieldType string
 
 const (
-	PRE_UPPER             FieldTyp = "pre_upper"
-	PRE_LOWER             FieldTyp = "pre_lower"
-	PRE_NUMBER            FieldTyp = "pre_number"
-	PRE_UNDERLINE         FieldTyp = "pre_underline"
-	PRE_MIDDLELINE        FieldTyp = "pre_middleline"
-	PRE_DOUBLE_UNDERLINE  FieldTyp = "pre_double_underline"
-	PRE_DOUBLE_MIDDLELINE FieldTyp = "pre_double_middleline"
-	PRE_SYMBOL            FieldTyp = "pre_symbol"
-	PRE_CHINESE           FieldTyp = "pre_chinese"
+	DEFAULT                  FieldType = "default"
+	UPPER                    FieldType = "upper"
+	LOWER                    FieldType = "lower"
+	NUMBER                   FieldType = "number"
+	UNDERLINE                FieldType = "underline"
+	MIDDLELINE               FieldType = "middleline"
+	SYMBOL                   FieldType = "symbol"
+	CHINESE                  FieldType = "chinese"
+	PRE_UPPER                FieldType = "pre_upper"
+	PRE_LOWER                FieldType = "pre_lower"
+	PRE_NUMBER               FieldType = "pre_number"
+	PRE_UNDERLINE            FieldType = "pre_underline"
+	PRE_MIDDLELINE           FieldType = "pre_middleline"
+	PRE_DOUBLE_UNDERLINE     FieldType = "pre_double_underline"
+	PRE_DOUBLE_MIDDLELINE    FieldType = "pre_double_middleline"
+	PRE_YMBOL                FieldType = "inside_symbol"
+	PRE_CHINESE              FieldType = "inside_chinese"
+	INSIDE_UPPER             FieldType = "inside_upper"
+	INSIDE_LOWER             FieldType = "inside_lower"
+	INSIDE_NUMBER            FieldType = "inside_number"
+	INSIDE_UNDERLINE         FieldType = "inside_underline"
+	INSIDE_MIDDLELINE        FieldType = "inside_middleline"
+	INSIDE_DOUBLE_UNDERLINE  FieldType = "inside_double_underline"
+	INSIDE_DOUBLE_MIDDLELINE FieldType = "inside_double_middleline"
+	INSIDE_SYMBOL            FieldType = "inside_symbol"
+	INSIDE_CHINESE           FieldType = "inside_chinese"
+	SUF_UPPER                FieldType = "suf_upper"
+	SUF_LOWER                FieldType = "suf_lower"
+	SUF_NUMBER               FieldType = "suf_number"
+	SUF_UNDERLINE            FieldType = "suf_underline"
+	SUF_MIDDLELINE           FieldType = "suf_middleline"
+	SUF_DOUBLE_UNDERLINE     FieldType = "suf_double_underline"
+	SUF_DOUBLE_MIDDLELINE    FieldType = "suf_double_middleline"
+	SUF_SYMBOL               FieldType = "suf_symbol"
+	SUF_CHINESE              FieldType = "suf_chinese"
 )
 
-var fieldList = map[string]map[FieldTyp]string{}
+var fieldList = map[string]map[FieldType]string{}
 
-func Register(typ FieldTyp, field, val string) {
+func Register(typ FieldType, field, val string) {
 	fields, ok := fieldList[field]
 	if !ok {
-		fields = make(map[FieldTyp]string)
+		fields = make(map[FieldType]string)
 		fieldList[field] = fields
 	}
 
 	fields[typ] = val
 }
 
-func GetFieldValue(typ FieldTyp, field string) (val string) {
+func GetFieldValue(typ FieldType, field string) (val string) {
 	if fields, ok := fieldList[field]; ok {
 		val = fields[typ]
 	}
